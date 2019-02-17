c95d8dcc3cdf57ffee313c322c31702906fdc548
prog2prog <s.saejia@gmail.com>
2019-02-17 21:54:45 +0700

    compiled

---
+++ pj_2019_MFC_by_Nextion_R01/src/L5_Foundation_2/NexionHardware.cpp
@@ -0,0 +1,219 @@
+#include "NextionHardware.h"
+
+#define NEX_RET_CMD_FINISHED            (0x01)
+#define NEX_RET_EVENT_LAUNCHED          (0x88)
+#define NEX_RET_EVENT_UPGRADED          (0x89)
+#define NEX_RET_EVENT_TOUCH_HEAD            (0x65)     
+#define NEX_RET_EVENT_POSITION_HEAD         (0x67)
+#define NEX_RET_EVENT_SLEEP_POSITION_HEAD   (0x68)
+#define NEX_RET_CURRENT_PAGE_ID_HEAD        (0x66)
+#define NEX_RET_STRING_HEAD                 (0x70)
+#define NEX_RET_NUMBER_HEAD                 (0x71)
+#define NEX_RET_INVALID_CMD             (0x00)
+#define NEX_RET_INVALID_COMPONENT_ID    (0x02)
+#define NEX_RET_INVALID_PAGE_ID         (0x03)
+#define NEX_RET_INVALID_PICTURE_ID      (0x04)
+#define NEX_RET_INVALID_FONT_ID         (0x05)
+#define NEX_RET_INVALID_BAUD            (0x11)
+#define NEX_RET_INVALID_VARIABLE        (0x1A)
+#define NEX_RET_INVALID_OPERATION       (0x1B)
+
+/*
+ * Receive uint32_t data. 
+ * 
+ * @param number - save uint32_t data. 
+ * @param timeout - set timeout time. 
+ *
+ * @retval true - success. 
+ * @retval false - failed.
+ *
+ */
+bool recvRetNumber(uint32_t *number, uint32_t timeout)
+{
+    bool ret = false;
+    uint8_t temp[8] = {0};
+
+    if (!number)
+    {
+        goto __return;
+    }
+    
+    Serial1.setTimeout(timeout);
+    if (sizeof(temp) != Serial1.readBytes((char *)temp, sizeof(temp)))
+    {
+        goto __return;
+    }
+
+    if (temp[0] == NEX_RET_NUMBER_HEAD
+        && temp[5] == 0xFF
+        && temp[6] == 0xFF
+        && temp[7] == 0xFF
+        )
+    {
+        *number = ((uint32_t)temp[4] << 24) | ((uint32_t)temp[3] << 16) | (temp[2] << 8) | (temp[1]);
+        ret = true;
+    }
+
+__return:
+
+    if (ret) 
+    {
+        Serial.print("recvRetNumber :");
+        Serial.println(*number);
+    }
+    else
+    {
+        Serial.println("recvRetNumber err");
+    }
+    
+    return ret;
+}
+
+
+/*
+ * Receive string data. 
+ * 
+ * @param buffer - save string data. 
+ * @param len - string buffer length. 
+ * @param timeout - set timeout time. 
+ *
+ * @return the length of string buffer.
+ *
+ */
+uint16_t recvRetString(char *buffer, uint16_t len, uint32_t timeout)
+{
+    uint16_t ret = 0;
+    bool str_start_flag = false;
+    uint8_t cnt_0xff = 0;
+    String temp = String("");
+    uint8_t c = 0;
+    long start;
+
+    if (!buffer || len == 0)
+    {
+        goto __return;
+    }
+    
+    start = millis();
+    while (millis() - start <= timeout)
+    {
+        while (Serial1.available())
+        {
+            c = Serial1.read();
+            if (str_start_flag)
+            {
+                if (0xFF == c)
+                {
+                    cnt_0xff++;                    
+                    if (cnt_0xff >= 3)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    temp += (char)c;
+                }
+            }
+            else if (NEX_RET_STRING_HEAD == c)
+            {
+                str_start_flag = true;
+            }
+        }
+        
+        if (cnt_0xff >= 3)
+        {
+            break;
+        }
+    }
+
+    ret = temp.length();
+    ret = ret > len ? len : ret;
+    strncpy(buffer, temp.c_str(), ret);
+    
+__return:
+
+    Serial.print("recvRetString[");
+    Serial.print(temp.length());
+    Serial.print(",");
+    Serial.print(temp);
+    Serial.println("]");
+
+    return ret;
+}
+
+/*
+ * Send command to Nextion.
+ *
+ * @param cmd - the string of command.
+ */
+void sendCommand(const char* cmd)
+{
+    while (Serial1.available())
+    {
+        Serial1.read();
+    }
+    
+    Serial1.print(cmd);
+    Serial1.write(0xFF);
+    Serial1.write(0xFF);
+    Serial1.write(0xFF);
+}
+
+
+/*
+ * Command is executed successfully. 
+ *
+ * @param timeout - set timeout time.
+ *
+ * @retval true - success.
+ * @retval false - failed. 
+ *
+ */
+bool recvRetCommandFinished(uint32_t timeout)
+{    
+    bool ret = false;
+    uint8_t temp[4] = {0};
+    
+    Serial1.setTimeout(timeout);
+    if (sizeof(temp) != Serial1.readBytes((char *)temp, sizeof(temp)))
+    {
+        ret = false;
+    }
+
+    if (temp[0] == NEX_RET_CMD_FINISHED
+        && temp[1] == 0xFF
+        && temp[2] == 0xFF
+        && temp[3] == 0xFF
+        )
+    {
+        ret = true;
+    }
+
+    if (ret) 
+    {
+        Serial.println("recvRetCommandFinished ok");
+    }
+    else
+    {
+        Serial.println("recvRetCommandFinished err");
+    }
+    
+    return ret;
+}
+
+bool systemInit(void)
+{
+    bool ret1 = false;
+    bool ret2 = false;
+    
+	Serial.begin(9600);
+    Serial1.begin(9600);
+    sendCommand("");
+    sendCommand("bkcmd=1");
+    ret1 = recvRetCommandFinished();
+    sendCommand("page 0");
+    ret2 = recvRetCommandFinished();
+    return ret1 && ret2;
+}
+
---
+++ pj_2019_MFC_by_Nextion_R01/src/L5_Foundation_2/NextionHardware.h
@@ -0,0 +1,10 @@
+#ifndef __NEXHARDWARE_H__
+#define __NEXHARDWARE_H__
+#include <Arduino.h>
+
+bool recvRetNumber(uint32_t *number, uint32_t timeout = 100);
+uint16_t recvRetString(char *buffer, uint16_t len, uint32_t timeout = 100);
+void sendCommand(const char* cmd);
+bool recvRetCommandFinished(uint32_t timeout = 100);
+
+#endif /* #ifndef __NEXHARDWARE_H__ */
